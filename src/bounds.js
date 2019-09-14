a2b568fb13498becd873cd3ac011c66bbd7fbfae
Vitor Silva <vitor.dasilvacruz@gmail.com>
2019-09-14 20:10:46 -0300

    feat: Add Possibility to init bound with no params (default settings)
    Close #1.

--- src/bounds.js
+++ src/bounds.js
@@ -1,121 +1,112 @@
 const checkForObserver = () => {
-  if (!('IntersectionObserver' in window)) {
+  if (!("IntersectionObserver" in window)) {
     throw new Error(`
       bounds.js requires IntersectionObserver on the global object.
       IntersectionObserver is unavailable in IE and other older
       versions of browsers.
       See https://github.com/ChrisCavs/bounds.js/blob/master/README.md
       for more compatibility information.
-    `)
+    `);
   }
-}
+};
 
 const getMargins = (margins = {}) => {
-  const {
-    top = 0,
-    right = 0,
-    bottom = 0,
-    left = 0
-  } = margins
-  return `${top}px ${right}px ${bottom}px ${left}px`
-}
+  const { top = 0, right = 0, bottom = 0, left = 0 } = margins;
+  return `${top}px ${right}px ${bottom}px ${left}px`;
+};
 
-const noOp = () => {}
+const noOp = () => {};
 
-const bound = (options) => {
-  return new Boundary(options)
-}
+const bound = (options = {}) => {
+  return new Boundary(options);
+};
 
 class Boundary {
-  constructor({root, margins, threshold, onEmit}) {
-    checkForObserver()
+  constructor({ root, margins, threshold, onEmit }) {
+    checkForObserver();
 
-    const marginString = getMargins(margins)
+    const rootMargin = getMargins(margins);
+    threshold = threshold || 0.0;
     const options = {
-      root: root || null,
-      rootMargin: marginString,
-      threshold: threshold || 0.0,
-    }
+      root,
+      rootMargin,
+      threshold
+    };
 
-    this.nodes = []
-    this.onEmit = onEmit || noOp
-    this.observer = new IntersectionObserver(
-      this._emit.bind(this),
-      options
-    )
+    this.nodes = [];
+    this.onEmit = onEmit || noOp;
+    this.observer = new IntersectionObserver(this._emit.bind(this), options);
   }
 
   // API
-  watch(el, onEnter=noOp, onLeave=noOp) {
+  watch(el, onEnter = noOp, onLeave = noOp) {
     const data = {
       el,
       onEnter,
-      onLeave,
-    }
+      onLeave
+    };
 
-    this.nodes.push(data)
-    this.observer.observe(el)
+    this.nodes.push(data);
+    this.observer.observe(el);
 
-    return data
+    return data;
   }
 
   unWatch(el) {
-    const index = this._findByNode(el, true)
+    const index = this._findByNode(el, true);
 
     if (index !== -1) {
-      this.nodes.splice(index, 1)
-      this.observer.unobserve(el)
+      this.nodes.splice(index, 1);
+      this.observer.unobserve(el);
     }
 
-    return this
+    return this;
   }
 
   check(el) {
-    const data = this._findByNode(el) || {}
-    return data.history
+    const data = this._findByNode(el) || {};
+    return data.history;
   }
 
   clear() {
-    this.nodes = []
-    this.observer.disconnect()
+    this.nodes = [];
+    this.observer.disconnect();
 
-    return this
+    return this;
   }
 
   static checkCompatibility() {
-    checkForObserver()
+    checkForObserver();
   }
 
   // HELPERS
   _emit(events) {
     const actions = events.map(event => {
-      const data = this._findByNode(event.target)
-      const ratio = event.intersectionRatio
+      const data = this._findByNode(event.target);
+      const ratio = event.intersectionRatio;
 
-      data.history = event.isIntersecting
+      data.history = event.isIntersecting;
 
-      event.isIntersecting
-        ? data.onEnter(ratio)
-        : data.onLeave(ratio)
+      event.isIntersecting ? data.onEnter(ratio) : data.onLeave(ratio);
 
       return {
         el: event.target,
         inside: event.isIntersecting,
         outside: !event.isIntersecting,
         ratio: event.intersectionRatio
-      }
-    })
+      };
+    });
 
-    this.onEmit(actions)
+    this.onEmit(actions);
   }
 
-  _findByNode(el, returnIndex=false) {
-    const func = returnIndex ? 'findIndex' : 'find'
+  _findByNode(el, returnIndex = false) {
+    const func = returnIndex ? "findIndex" : "find";
 
     return this.nodes[func](node => {
-      return node.el.isEqualNode(el)
-    })
+      return node.el.isEqualNode(el);
+    });
   }
 }
 
-export default bound
+export default bound;
